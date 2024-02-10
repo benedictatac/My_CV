6cf07073b7cdd905cd7bad7e30fadbfb19a5824f
benedictatac <atanaciocharles@yahoo.com>
2024-02-09 22:59:18 -0500

    trying to make page work
    Still hasnt worked

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,11 +1,12 @@
-import styles from "../styles/page.module.css";
-import NavBar from "../../components/navbar";
+import Navbar from "../../components/navbar";
+import styles from "../../styles/globals.css"
+export {default as NavBar} from "../../components/navbar";
 
 
 export default function Home() {
   return (
     <div className="main-page flex flex-col h-screen">
-      <NavBar />
+      <Navbar/>
       <main className={`${styles.main} flex-grow`}>
       </main>
     </div>
