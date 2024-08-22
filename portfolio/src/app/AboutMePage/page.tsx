852e739f17143d98e137ba87e805b7a0f1b4538f
Charles Benedict <atanaciocharles@yahoo.com>
2024-08-22 14:22:30 -0400

    created a navbar and 4 different pages

---
+++ portfolio/src/app/AboutMePage/page.tsx
@@ -0,0 +1,14 @@
+import "../../../styles/globals.css"; // Import global styles correctly
+
+export default function AboutMePage() {
+  return (
+    <div className="about-page flex flex-col h-screen">
+      <main className="flex-grow">
+        {/* Content specific to the About Me page */}
+        <h1 className="text-center text-xl text-black">About Me</h1>
+        <h2>This page is going to be about me </h2>
+        {/* Additional content goes here */}
+      </main>
+    </div>
+  );
+}
