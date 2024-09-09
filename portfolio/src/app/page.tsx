7fc569bd3669db1a7ede5725506e2291b79f75aa
Charles Benedict <atanaciocharles@yahoo.com>
2024-09-08 22:18:02 -0400

    updating home page

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -5,13 +5,16 @@ export default function HomePage() {
     <div className="home-page flex flex-col h-screen">
       <main className="flex-grow">
         {/* Content specific to the Home page */}
-        <div className="text-center text-xl text-white">My Portfolio  </div>
+        <div className="text-center text-xl text-white">  </div>
         <div className="flex items-center justify-center h-screen">
-          <p className="text-center text-lg">
-    Hello Ladies And Gents! My name is <h1 className="text-[20px] ">Charles Benedict Villafuerte Atanacio </h1> and this is 
-    my Portfolio
-  </p>
-</div>
+          <p className="mb-3 text-gray-500 dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:me-3 first-letter:float-start">
+    Hello Ladies And Gents! My name is <em>Charles Benedict Villafuerte Atanacio</em> and this is 
+    my Portfolio showcasing whatever talent I have remaining
+    <p className="text-gray-500 dark:text-gray-400">I am not a yapper but for this one, i shall for just a tiny bit. 
+      <p>Hello
+      </p></p></p>
+    
+    </div>
           
           
         
