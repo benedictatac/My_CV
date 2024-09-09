f1e59c45020d3982a8bb05efa52503e789e47485
Charles Benedict <atanaciocharles@yahoo.com>
2024-09-09 10:47:49 -0400

    just updated the paragraph so it is one below the other
    Also put them in a div

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -5,21 +5,19 @@ export default function HomePage() {
     <div className="home-page flex flex-col h-screen">
       <main className="flex-grow">
         {/* Content specific to the Home page */}
-        <div className="text-center text-xl text-white">  </div>
-        <div className="flex items-center justify-center h-screen">
-          <p className="mb-3 text-gray-500 dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:me-3 first-letter:float-start">
+        <div className="flex items-center justify-center h-screen p-6">
+          <div className="p-4">
+          <p className="mb-3 text-gray-500 dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:me-3 first-letter:float-start ">
     Hello Ladies And Gents! My name is <em>Charles Benedict Villafuerte Atanacio</em> and this is 
-    my Portfolio showcasing whatever talent I have remaining
-    <p className="text-gray-500 dark:text-gray-400">I am not a yapper but for this one, i shall for just a tiny bit. 
-      <p>Hello
-      </p></p></p>
+    my Portfolio showcasing whatever talent I have remaining</p>
+          <p className="text-gray-500 dark:text-gray-400">I am not a yapper but for this one, i shall for just a tiny bit.</p>          
+          {/*In this case, in order to get pargraphs below one another, must be in a div
+          If not, */}
+
+          </div>
+    </div> 
+      </main> 
+    </div>
     
-    </div>
-          
-          
-        
-        {/* Additional content goes here */}
-      </main>
-    </div>
   );
 }
