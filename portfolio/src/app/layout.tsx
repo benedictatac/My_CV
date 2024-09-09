c823bde3892e44152d7d321b98a52a126d82349f
Charles Benedict <atanaciocharles@yahoo.com>
2024-09-08 21:30:41 -0400

    created a small footer and started main home page
    added a small paragraph

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../../styles/globals.css";
-import Navbar from "../app/components/navbar"; // Adjusted the import path
-
+import Header from "./components/header";
+import Footer from "./components/footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,8 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar /> {/* Navbar is included here */}
+        <Header /> {/* Navbar is included here */}
         {children}
+        <Footer/>
       </body>
     </html>
   );
--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -5,8 +5,16 @@ export default function HomePage() {
     <div className="home-page flex flex-col h-screen">
       <main className="flex-grow">
         {/* Content specific to the Home page */}
-        <h1 className="text-center text-xl text-black">Home Page</h1>
-        <h2> This is going to be my Home Page </h2>
+        <div className="text-center text-xl text-white">My Portfolio  </div>
+        <div className="flex items-center justify-center h-screen">
+          <p className="text-center text-lg">
+    Hello Ladies And Gents! My name is <h1 className="text-[20px] ">Charles Benedict Villafuerte Atanacio </h1> and this is 
+    my Portfolio
+  </p>
+</div>
+          
+          
+        
         {/* Additional content goes here */}
       </main>
     </div>
