6be67f94ae111091bb8b313b21374c4976bdbebb
Charles Benedict <atanaciocharles@yahoo.com>
2024-09-11 09:14:05 -0400

    Style Changes
    Changed background color
    text changes
    and blended header and footer to bg

--- portfolio/src/app/components/footer.tsx
+++ portfolio/src/app/components/footer.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-black text-white py-4">
+    <footer className="bg-transparent text-white py-4">   {/*We wanna blend in the footer to the background color so make it transparent*/}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center">
           <div>
-            <p className="text-sm">Phone: (514) 690-7641</p>
-            <p className="text-sm">Email: atanaciocharles@yahoo.ca/atanaciocharles31@gmail.com</p>
+            <p className="text-sm text-gray-500 dark:text-gray-400">Phone: (514)-690-7641</p>
+            <p className="text-sm text-gray-500 dark:text-gray-400">Email: atanacocharles@yahoo.ca/atanaciocharles31@gmail.com</p>
           </div>
         </div>
       </div>
--- portfolio/src/app/components/header.tsx
+++ portfolio/src/app/components/header.tsx
@@ -4,9 +4,12 @@ import Navbar from "./navbar"; // Import the Navbar
 const Header: React.FC = () => {
   return (
     <React.Fragment>
+      <header className="bg-transparent"> {/* same logic from footer applies here for bg-color */}
       {/* You can add additional header content here if needed */}
       <Navbar /> {/* Navbar is part of the header */}
+      </header>
     </React.Fragment>
+    
   );
 };
 
