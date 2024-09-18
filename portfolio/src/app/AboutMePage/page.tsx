cec33ab24bb61ce057c7aca37d5b493cdc4d34ba
Charles Benedict <atanaciocharles@yahoo.com>
2024-09-18 00:09:04 -0400

    cleaned up main page
    removed redu3ndancy of divs and added:
    section tags
    article tags

--- portfolio/src/app/AboutMePage/page.tsx
+++ portfolio/src/app/AboutMePage/page.tsx
@@ -4,15 +4,6 @@ export default function AboutMePage() {
   return (
     <div className="about-page flex flex-col h-screen">
       <main className="flex-grow">
-        <div className="flex-grow">
-          <div className="flex items-center justify center h-screen p-6">
-          <div >
-          </div>
-
-          </div>
-
-        </div>
-
       </main>
     </div>
   );
