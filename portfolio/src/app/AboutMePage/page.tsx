56e7f76155e2cc72f5047e10382315806c5f3115
Charles Benedict Villafuerte Atanacio <atanaciocharles31@gmail.com>
2024-10-06 21:05:18 -0400

    changed bg color and buttons for slide

--- portfolio/src/app/AboutMePage/page.tsx
+++ portfolio/src/app/AboutMePage/page.tsx
@@ -8,12 +8,12 @@ export default function AboutMePage() {
       <main className="flex-grow">
       <div className="grid grid-cols-1 row-span-1">
         <h1 className="text-white">Who am I</h1>
-      <section className="bg-gray-200 p-4 flex justify-between items-center h-96 mt-8">
-      <div className="flex-1 bg-slate-900 h-full">
+      <section className="bg-transparent p-4 flex justify-between items-center h-96 mt-8">
+      <div className="flex-1 bg-transparent h-full">
       <p>This will contain some photos of me that are slidable</p>
       <Carousel/>
       </div>
-      <div className="bg-black h-full w-1/4">
+      <div className="bg-transparent h-full w-1/4">
       <p>This section will contain some type of text</p>
       </div>
       </section>
