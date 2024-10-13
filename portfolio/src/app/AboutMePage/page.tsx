f3c3706312a24baa77efbaa42848d8767c38d934
benedictatac <atanaciocharles@yahoo.com>
2024-10-12 21:21:48 -0400

    Add new images and other untracked files

--- portfolio/src/app/AboutMePage/page.tsx
+++ portfolio/src/app/AboutMePage/page.tsx
@@ -9,12 +9,13 @@ export default function AboutMePage() {
       <div className="grid grid-cols-1 row-span-1">
         <h1 className="text-white">Who am I</h1>
       <section className="bg-transparent p-4 flex justify-between items-center h-96 mt-8">
-      <div className="flex-1 bg-transparent h-full">
+      <div className="flex-1 bg-transparent h-full mr-20">
       <p>This will contain some photos of me that are slidable</p>
       <Carousel/>
       </div>
-      <div className="bg-transparent h-full w-1/4">
-      <p>This section will contain some type of text</p>
+      <div className="bg-transparent h-full w-2/5">
+      <p className="text-gray-500 dark:text-gray-400 ">Who am I you ask? Well if you did ask, My name is <text className="italic">Charles Benedict Villafuerte Atanacio.</text></p>
+      <p className="text-gray-500 dark:text-gray-400">I am a <text className = "italic">Computer Engineering Student</text> trying to survive in this economy </p>
       </div>
       </section>
 
