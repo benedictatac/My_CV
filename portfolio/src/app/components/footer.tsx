2c8b15b64f01ddffa421afb2d2491cdfbeb6ef71
Charles Benedict Villafuerte Atanacio <atanaciocharles31@gmail.com>
2024-10-14 20:29:38 -0400

    added styling to prevent overflow
    images are now cropped and showing responsively

--- portfolio/src/app/components/footer.tsx
+++ portfolio/src/app/components/footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-transparent text-white py-4">   {/*We wanna blend in the footer to the background color so make it transparent*/}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center ">
           <div>
             <p className="text-sm text-gray-500 dark:text-gray-400">Phone: (514)-690-7641</p>
             <p className="text-sm text-gray-500 dark:text-gray-400">Email: atanacocharles@yahoo.ca/atanaciocharles31@gmail.com</p>
