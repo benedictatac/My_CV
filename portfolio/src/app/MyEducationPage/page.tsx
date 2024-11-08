58d099a92dbb7a6b83b59eee53d6e6cb1f095e0f
benedictatac <atanaciocharles@yahoo.com>
2024-11-08 18:05:02 -0500

    installed css modules
    Started dividing sections for education page
    NOTE: peter said, BE YOURSELF

--- portfolio/src/app/MyEducationPage/page.tsx
+++ portfolio/src/app/MyEducationPage/page.tsx
@@ -2,12 +2,8 @@ import "../../../styles/globals.css"; // Import global styles correctly
 
 export default function AboutMePage() {
   return (
-    <div className="about-page flex flex-col h-screen">
+    <div className="about-page flex flex-col min-h-screen" style={{ height: '100vh', overflowY: 'scroll', paddingRight: '10px' }}>
       <main className="flex-grow">
-        {/* Content specific to the About Me page */}
-        <h1 className="text-center text-xl text-black">About Me</h1>
-        <h2>This page is going to be about my Education </h2>
-        {/* Additional content goes here */}
       </main>
     </div>
   );
