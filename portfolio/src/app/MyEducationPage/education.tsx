a5beba9c4e5615afe5d2b5c5b4aa28a5d070f572
benedictatac <atanaciocharles@yahoo.com>
2024-11-08 18:05:02 -0500

    installed css modules
    Started dividing sections for education page
    NOTE: peter said, BE YOURSELF

---
+++ portfolio/src/app/MyEducationPage/education.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+//since its only a page that will be used
+// dont need main
+
+export default function Education()
+{
+    return(
+        <div className="grid row-span-1 col-span-1">
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+        </div>
+
+    )
+}
