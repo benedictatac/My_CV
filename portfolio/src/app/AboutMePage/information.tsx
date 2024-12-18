1436513f0cc890392f02947513adfa983c7814e2
Charles Benedict Villafuerte Atanacio <atanaciocharles31@gmail.com>
2024-12-18 00:53:37 -0500

    removed intro from about me and added a new section

--- portfolio/src/app/AboutMePage/information.tsx
+++ portfolio/src/app/AboutMePage/information.tsx
@@ -4,22 +4,6 @@ import Carousel from "../components/carousel";
 export default function AboutMePage() {
 return(
     <div className="grid grid-cols-1 row-span-1">
-    {/* Section 1 */}
-    <section className="min-h-screen flex flex-col justify-center items-center mr-auto ml-auto">
-      <div className="mb-48">
-      <h1 className="mb-4 text-4xl text-center font-extrabold leading-none tracking-tight bg-gradient-to-r from-[#0e1c26] to-[#bbcace] text-transparent bg-clip-text md:text-5xl lg:text-6xl dark:from-[#020303] dark:to-[#3295b1]">
-        Who am I
-      </h1>
-      <div className="text-center font-bold">
-        <p className="text-gray-500 dark:text-gray-400">
-          Who am I you ask? Well if you did ask, My name is <span className="italic">Charles Benedict Villafuerte Atanacio.</span>
-        </p>
-        <p className="text-gray-500 dark:text-gray-400 text-center">
-          I am a {<span className="italic">Computer Engineering Student</span>} trying to survive in this economy! Lemme show you
-        </p>
-      </div>
-      </div>
-    </section>
     {/* Section 2 */}
     <section className="min-h-screen flex flex-col justify-center items-center w-auto">
       <div className="flex flex-row justify-between w-full px-10 sm:px-20">
---
+++ portfolio/src/app/AboutMePage/introduction.tsx
@@ -0,0 +1,16 @@
+export default function Introduction() {
+return (    
+<div>
+<h1 className="mb-4 text-4xl text-center font-extrabold leading-none tracking-tight bg-gradient-to-r from-[#0e1c26] to-[#bbcace] text-transparent bg-clip-text md:text-5xl lg:text-6xl dark:from-[#020303] dark:to-[#3295b1]">
+  Who am I
+</h1>
+<div className="text-center font-bold">
+  <p className="text-gray-500 dark:text-gray-400">
+    Who am I you ask? Well if you did ask, My name is <span className="italic">Charles Benedict Villafuerte Atanacio.</span>
+  </p>
+  <p className="text-gray-500 dark:text-gray-400 text-center">
+    I am a {<span className="italic">Computer Engineering Student</span>} trying to survive in this economy! Lemme show you
+  </p>
+</div>
+</div>
+);}
