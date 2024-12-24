b5fe9a70a190a602a50e4f793db85d82eb86f028
benedictatac <atanaciocharles@yahoo.com>
2024-12-23 21:00:04 -0500

    adding new content

--- portfolio/src/app/components/hoverReveal.tsx
+++ portfolio/src/app/components/hoverReveal.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from '../styles/hoverReveal.module.css';
 
-const HoverReveal: React.FC = () => {
+
+interface HoverRevealProps {
+  title: string;
+  content: string;
+}
+const HoverReveal: React.FC<HoverRevealProps> = ({title, content}) => {
   return (
     <div className={styles.container}>
-      <h2 className={styles.revealText}>Highschool</h2>
+      <h2 className={styles.revealText}>{title}</h2>
       <div className={styles.revealContent}>
         <p>
-          I went to XYZ Highschool, a school focused on academic excellence and
-          extracurricular activities. It was a great experience!
+          {content}
         </p>
       </div>
     </div>
