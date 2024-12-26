02e48fa31225de713602af1e176bd00f3aaac891
benedictatac <atanaciocharles@yahoo.com>
2024-12-25 19:08:22 -0500

    adding new section for experience

--- portfolio/src/MyExperiencePage/experience.tsx
+++ portfolio/src/MyExperiencePage/experience.tsx
@@ -1,22 +1,35 @@
 import "../app/styles/experience.css";
 
-
-export default function Experience()
-{
+export default function Experience() {
     return (
-        <div className = "flex-grow justify-center min-h-screen">
+        <div className="flex-grow justify-center min-h-screen">
             <div className="title">
-            <h1 className="mb-4 text-4xl text-center font-extrabold leading-none tracking-tight bg-gradient-to-r from-[#0e1c26] to-[#bbcace] text-transparent bg-clip-text md:text-5xl lg:text-6xl dark:from-[#020303] dark:to-[#3295b1] p-4 relative">
-          <span className="relative z-10 bg-gradient-to-r from-[#0e1c26] to-[#21b8e2] bg-clip-text text-transparent animate-pulse">Experience</span>
-        </h1>
+                <h1 className="mb-4 text-4xl text-center font-extrabold leading-none tracking-tight bg-gradient-to-r from-[#0e1c26] to-[#bbcace] text-transparent bg-clip-text md:text-5xl lg:text-6xl dark:from-[#020303] dark:to-[#3295b1] p-4 relative">
+                    <span className="relative z-10 bg-gradient-to-r from-[#0e1c26] to-[#21b8e2] bg-clip-text text-transparent animate-pulse">Experience</span>
+                </h1>
             </div>
-            <div className = "content">
-                <div className="marios"></div>
-                <div className="uni-select"></div>
+            <div className="content">
+                <div className="marios">
+                    <img src="Pictures/Marios.jpg" className="marios-img" alt="Marios Picture"></img>
+                    <div className="marios-content">
+                    <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Job Titles</h1>
+                        <ul className="list-disc list-inside">
+                            <br/>
+                        <li>
+                        <span className="text-lg font-bold">Cook</span>
+                        </li>
+                        </ul>
+
+
+                    </div>
+                </div>
+                <div className="uniSelect">
+                    <img src="Pictures/planchers_GSM.png" className="uniSelect-img" alt="Uni-SelectPicture"></img>
+                    <div></div>
+                </div>
                 <div className="gsm"></div>
                 <div className="matrox"></div>
             </div>
         </div>
-    )
-
+    );
 }
--- portfolio/src/app/components/navbar.tsx
+++ portfolio/src/app/components/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"; // Import Link from next/link
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="fixed top-0 right-0 w-full bg-transparent">
+    <nav className="fixed top-0 right-0 w-full bg-transparent z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
