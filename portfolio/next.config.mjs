daaceb261a27b14929d658638d64f6b5e958eb05
charles <atanaciocharles31@gmail.com>
2025-01-12 23:45:35 -0500

    changfed path for next.js so vercel can find it

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+version https://git-lfs.github.com/spec/v1
+oid sha256:328698fb56dfab70945005eeadbda20948fc8d3acb2eef8266ddfbeb68d44d4c
+size 258
