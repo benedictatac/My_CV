cafb8c7571b347d7bddb3f6f648c2a0c9b09801b
charles <atanaciocharles31@gmail.com>
2025-01-13 07:30:43 -0500

    changes

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,2 +1,10 @@
-
-export default nextConfig;
+// next.config.mjs
+const nextConfig = {
+    reactStrictMode: true,  // Enables React Strict Mode for additional checks and warnings
+    eslint: {
+      ignoreDuringBuilds: true  // Temporarily ignores ESLint during builds
+    }
+  };
+  
+  export default nextConfig;
+  
