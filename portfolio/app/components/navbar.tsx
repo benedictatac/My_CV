3363595cd6eeeca022027ea3fd6bce93ee299d20
Charles Benedict Villafuerte Atanacio <atanaciocharles31@gmail.com>
2025-02-26 10:23:37 -0500

    changing navbar structure, keeping it simple for now

--- portfolio/app/components/navbar.tsx
+++ portfolio/app/components/navbar.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Link from "next/link"; // Import Link from next/link
+import "../styles/header.css"
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="fixed top-0 right-0 w-full z-50 bg-black-500 dark:bg-gray-1200 shadow-lg">
+    <nav className="fixed top-0 right-0 w-full z-50 bg-black-500 dark:bg-gray-1200 shadow-lg ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-4 ">
         <div className="flex items-center justify-between h-16 ">
           <div className="flex items-center">
@@ -13,33 +14,33 @@ const Navbar: React.FC = () => {
           </div>
           <div className="hidden md:block">
             <div className="ml-4 flex items-center space-x-4" >
-              <div className="HomeButton">
-              <button>
-                <Link href="#home" className="text-gray-500 dark:text-gray-400 hover:bg-transparent hover:text-white rounded-lg" >
+              <div>
+                <Link href="#home"  >
+                  <button  className = "navBarButtons">
                     <span style={{ color: "white", fontWeight: "bold" }}>Home</span>
+                  </button>
                 </Link>
-              </button>
               </div>
-              <div className="AboutButton">
-              <button>
+              <div className="navBarButtons">
               <Link href="#aboutme"  className="text-gray-500 dark:text-gray-400 hover:bg-transparent hover:text-white rounded-lg">
+              <button> 
                 <span style = {{color : "white", fontWeight:"bold"}}>About Me</span>
-              </Link>
               </button>
+              </Link>
               </div>
-             <div className="EducationButton">
+             <div >
+             <Link href="#myeducation" className="navBarButtons">
              <button>
-              <Link href="#myeducation" className="text-gray-500 dark:text-gray-400 hover:bg-transparent hover:text-white rounded-lg">
               <span style = {{color : "white", fontWeight:"bold"}}>Education</span>
-              </Link>
               </button>
+              </Link>
              </div>
-             <div className="ExperienceButton">
+             <div>
+             <Link href="#myExperience" className="navBarButtons">
              <button>
-              <Link href="#myExperience" className="text-gray-500 dark:text-gray-400 hover:bg-transparent hover:text-white rounded-lg">
               <span style = {{color : "white", fontWeight:"bold"}}>Experience</span>
-              </Link>
               </button>
+              </Link>
              </div>
             </div>
           </div>
