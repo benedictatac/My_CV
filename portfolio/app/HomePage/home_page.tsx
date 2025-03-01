f73e547a1c283d0e37862fc12a622ae723b53a66
Charles Benedict Villafuerte Atanacio <atanaciocharles31@gmail.com>
2025-02-28 20:52:02 -0500

    removed background name and added paragraph for homepage

--- portfolio/app/HomePage/home_page.tsx
+++ portfolio/app/HomePage/home_page.tsx
@@ -5,12 +5,19 @@ import "../styles/home_page.css"
 export default function HomePage(){
 return (
     <div className="flex items-start justify-start min-h-screen p-6">
-    <div className ="grid grid-rows-2 w-1/2 min-h-screen bg-black p-6">
-    <div className="p-16 rows-span-2">
-        <h1 className="mb-4">Aspirting Computer Engineering Student</h1>   
+    <div className ="grid grid-cols-auto grid-rows-auto w-2/3 bg-transparent p-6">
+    <div className="p-4 col-span-3">
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Computer Engineering Student</h1>
         </div>
-    <div className="p-8 rows-span-2 h-full"></div>
+        <div className="p-4 col-span-3">
+            <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl  dark:text-gray-400 w-fit">
+                My name is Charles Atanacio - an aspiring student eager to learn in the sphere of <span className="text-4x1 font-bold text-cyan-500">Software Development</span> based in Montreal, Quebec Canada.  
+                {/* <span className="text-4x1 font-bold text-cyan-500"> <u>Web Development, Game Development and Artificial Intelligence. </u></span> */} I hold under my belt, multiple years of experience in part-time jobs while being a full-time student. 
+                I am currently in my last year of my  <span className="text-4x1 font-bold text-cyan-500"> Computer Engineering Degree </span> at Concordia University.
+                I knew nothing before I got into engineering but now...I know more! Hit me up 😃
+            </p>
+        </div>
     </div>
-    <div className="grid grid-cols-2 flex-1 min-h-screen bg-white p-6"></div>
+    <div className="flex-1 min-h-screen bg-transparent p-6"> {/*will include logos */}</div>
     </div>
 )}
--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -11,9 +11,9 @@ export default function mainPage(){
     // the entire height of the screen 
     // through a column based setting 
     <div className="flex flex-col min-h-screen" style = {{height :'100vg', overflowY: 'scroll'}}>
-      <div className="absolute inset-0 z-0 flex justify-center items-center text-[10vw] font-extrabold text-white opacity-5 pointer-events-none select-none font-manjeet">
+      {/* <div className="absolute inset-0 z-0 flex justify-center items-center text-[10vw] font-extrabold text-white opacity-5 pointer-events-none select-none font-manjeet">
         Charles Benedict Villafuerte Atanacio 
-      </div>
+      </div> */}
       <main className="flex-grow"> 
         <div className="flex-grow grid-cols-1 row-span-1">
         <section id = "home" className="flex-grow justify-center">
